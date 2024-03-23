fa5a0b284d74e893ba84eba4eaf718fdbafe6d32
arjun-bhalekar <arjunbhalekar95@gmail.com>
2024-03-23 12:35:15 +0530

    inital code

---
+++ src/main/java/com/sb/app/company/ms/CompanyMicroServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sb.app.company.ms;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CompanyMicroServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CompanyMicroServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sb/app/company/ms/CompanyMicroServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sb.app.company.ms;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CompanyMicroServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
