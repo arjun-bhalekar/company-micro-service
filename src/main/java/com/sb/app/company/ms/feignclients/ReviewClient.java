0fed921746f0280f650aa9dd0c7d8b62c282768f
arjun-bhalekar <arjunbhalekar95@gmail.com>
2024-03-31 19:35:55 +0530

    containerized(docker-compose) spring boot application

--- src/main/java/com/sb/app/company/ms/feignclients/ReviewClient.java
+++ src/main/java/com/sb/app/company/ms/feignclients/ReviewClient.java
@@ -4,7 +4,7 @@ import org.springframework.cloud.openfeign.FeignClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
-@FeignClient(name = "REVIEW-MICRO-SERVICE")
+@FeignClient(name = "REVIEW-MICRO-SERVICE", url = "${review-service.url}")
 public interface ReviewClient {
 
     @GetMapping("/reviews/averageRating")
